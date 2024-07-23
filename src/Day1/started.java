756589f8170d14f65e5cfbad072d5e273dbd03d1
Aayush Shrestha <aayushstha20@gmail.com>
2024-07-23 16:20:02 +0545

    Getting statrted

---
+++ src/Day1/started.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class started {
+	public static void main(String[] args) {
+		int a = 1, b = 2;
+		var c = "Aayush";
+		System.out.println("Hello World!!");
+		System.out.print(a+b);
+		System.out.print("\n"+c);
+	}
+}
--- src/patterns.java
+++
@@ -1,9 +0,0 @@
-
-public class patterns {
-
-	public static void main(String[] args) {
-		System.out.println("Hello world!!");
-
-	}
-
-}
