9c26bbf88fa9a8b58d1a18de5739e8ecc79c1a34
Aayush Shrestha <aayushstha20@gmail.com>
2024-07-23 16:46:52 +0545

    Refernce datatypes

---
+++ src/Day1/refDatatypes.java
@@ -0,0 +1,19 @@
+package Day1;
+
+class Car{
+	int sn;
+	String name;
+	double price;
+}
+
+public class refDatatypes {
+	public static void main(String [] args) {
+		Car m1 = new Car();
+		m1.sn=230;
+		m1.name="BMW";
+		m1.price=100000.34;
+		
+		System.out.println("Car description"
+				+ "\nSN: "+m1.sn+"\n"+m1.name+"\nNrs. "+ m1.price);
+	}
+}
