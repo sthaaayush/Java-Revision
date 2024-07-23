022430ceeb4acc821c1611840e91af191bccd19d
Aayush Shrestha <aayushstha20@gmail.com>
2024-07-23 16:39:39 +0545

    Primitive datatypes

---
+++ src/Day1/primDatatypes.java
@@ -0,0 +1,23 @@
+package Day1;
+
+public class primDatatypes {
+	public static void main(String[] args){
+		byte b=1;
+		short s=2;
+		int i=3;
+		long l=4;
+		float f= 4.5f;
+		char c='5';
+		double d=5.5;
+		boolean bo=true;
+		System.out.println(b + "\nType: "+ ((Object)b).getClass().getName() + " \nSize: "+ Byte.SIZE);
+		System.out.println(s + "\nType: "+ ((Object)s).getClass().getName() + " \nSize: "+ Short.SIZE);
+		System.out.println(i + "\nType: "+ ((Object)i).getClass().getName() + " \nSize: "+ Integer.SIZE);
+		System.out.println(l + "\nType: "+ ((Object)l).getClass().getName() + " \nSize: "+ Long.SIZE);
+		System.out.println(f + "\nType: "+ ((Object)f).getClass().getName() + " \nSize: "+ Float.SIZE);
+		System.out.println(c + "\nType: "+ ((Object)c).getClass().getName() + " \nSize: "+ Character.SIZE);
+		System.out.println(d + "\nType: "+ ((Object)d).getClass().getName() + " \nSize: "+ Double.SIZE);
+		System.out.println(bo + "\nType: "+ ((Object)bo).getClass().getName() + " \nSize: 1");
+		
+	}
+}
--- src/Day1/started.java
+++ src/Day1/started.java
@@ -3,7 +3,7 @@ package Day1;
 public class started {
 	public static void main(String[] args) {
 		int a = 1, b = 2;
-		var c = "Aayush";
+		var c = "Aayush"; //feature of jdk10 
 		System.out.println("Hello World!!");
 		System.out.print(a+b);
 		System.out.print("\n"+c);
