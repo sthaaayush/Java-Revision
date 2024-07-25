b44992c16e5c992f9427b21e8e2a67184b68d902
Aayush Shrestha <aayushstha20@gmail.com>
2024-07-25 20:24:53 +0545

    Custom Exception

---
+++ src/day3/CustomeException.java
@@ -0,0 +1,25 @@
+package day3;
+
+class NullValueException extends Exception{
+	public NullValueException(String msg) {
+		super(msg);
+	}
+}
+
+public class CustomeException {
+	public static void arrDisplay(String name) throws NullValueException{
+		if (name==null || name.isEmpty()) {
+			throw new NullValueException("Name cannot be empty");
+		}else {
+			System.out.println("Hello, "+name);
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			arrDisplay("");
+		} catch (NullValueException e) {
+			System.out.println("Exception caught:"+ e.getMessage());
+		}
+	}
+}
