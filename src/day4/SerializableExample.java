b18928a728dd3d35ff788198d23c4fd53277b1e3
Aayush Shrestha <aayushstha20@gmail.com>
2024-07-27 16:19:20 +0545

    Implemeted Serializable

---
+++ src/day4/SerializableExample.java
@@ -0,0 +1,45 @@
+package day4;
+
+import java.io.*;
+
+class Car implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private float price;
+
+	public Car(String name, float price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
+
+public class SerializableExample {
+	public static void main(String[] args) {
+		Car c1 = new Car("BMW", 20000.45f);
+		//Serializing Object
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("car.ser"))) {
+			oos.writeObject(c1);
+			System.out.println("Object serialized");
+		} catch (Exception e) {
+			System.out.println("Exception occur: " + e.getMessage());
+		}
+		
+		//Deserializing 
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("car.ser"))){
+			Car c2=(Car)ois.readObject();
+			System.out.println("Object deserialized: "+ c2);
+			
+		}catch (Exception e) {
+			System.out.println("Exception occur: " + e.getMessage());
+		}
+	}
+}
