00699627dce360e715ccf6b34356ceae79f1f149
Aayush Shrestha <aayushstha20@gmail.com>
2024-07-29 20:32:39 +0545

    Few enhanchments in Vector and Stack

--- src/day5/VectorExample.java
+++ src/day5/VectorExample.java
@@ -3,25 +3,27 @@ package day5;
 import java.util.Vector;
 
 public class VectorExample {
-	public static void main(String[] args) {
-		Vector<String> place=new Vector<>();
-		
-		place.add("Kathmandu");
-		place.add("Pokhara");
-		place.add("Dharan");
-		place.add("Bhaktapur");
-		
-		System.out.println("Original list");
-		printList(place); 
-		
-		System.out.println("\nRemoving elements at [2]");
-		place.remove(2);
-		printList(place);
-	}
-	
-	private static void printList(Vector<String> list) {
-		for(String a: list) {
-			System.out.println(a);                    
-		}
-	}
+    public static void main(String[] args) {
+        Vector<String> place = new Vector<>();
+
+        place.add("Kathmandu");
+        place.add("Pokhara");
+        place.add("Dharan");
+        place.add("Bhaktapur");
+
+        System.out.println("Original list:");
+        printList(place);
+
+        System.out.println("\nRemoving element at index [2]");
+        place.remove(2);
+        printList(place);
+
+        System.out.println("\nVector size: " + place.size());
+    }
+
+    private static void printList(Vector<String> list) {
+        for (String a : list) {
+            System.out.println(a);
+        }
+    }
 }
