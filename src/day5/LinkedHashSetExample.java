857debb31c172c12b079715139e441c49948a353
Aayush Shrestha <aayushstha20@gmail.com>
2024-08-01 16:33:48 +0545

    Added comments for readability

--- src/day5/LinkedHashSetExample.java
+++ src/day5/LinkedHashSetExample.java
@@ -2,18 +2,22 @@ package day5;
 
 import java.util.LinkedHashSet;
 
-public class LinkedHashSetExample{
-	public static void main(String[] args) {
-		LinkedHashSet<Character> chars=new LinkedHashSet<>();
-		
-		chars.add('A');
-		chars.add('C');
-		chars.add('B');
-		chars.add('E');
-		chars.add('D');
-		
-		for(Character a: chars) {
-			System.out.println(a);
-		}
-	}
+public class LinkedHashSetExample {
+    public static void main(String[] args) {
+        // Create a LinkedHashSet of Character type
+        LinkedHashSet<Character> chars = new LinkedHashSet<>();
+
+        // Adding elements to the LinkedHashSet
+        chars.add('A');
+        chars.add('C');
+        chars.add('B');
+        chars.add('E');
+        chars.add('D');
+
+        // Displaying the elements in the LinkedHashSet
+        System.out.println("LinkedHashSet elements:");
+        for (Character a : chars) {
+            System.out.println(a);
+        }
+    }
 }
--- src/day5/TreeSetExample.java
+++ src/day5/TreeSetExample.java
@@ -3,17 +3,27 @@ package day5;
 import java.util.TreeSet;
 
 public class TreeSetExample {
-	public static void main(String[] args) {
-		TreeSet<Integer> num=new TreeSet<Integer>();
-		
-		num.add(15);
-		num.add(30);
-		num.add(25);
-		num.add(5);
-		num.add(40);
-		
-		for(Integer a: num) {
-			System.out.println(a);
-		}
-	}
+    public static void main(String[] args) {
+        // Create a TreeSet of Integer type
+        TreeSet<Integer> num = new TreeSet<>();
+
+        // Adding elements to the TreeSet
+        num.add(15);
+        num.add(30);
+        num.add(25);
+        num.add(5);
+        num.add(40);
+
+        // Displaying elements in natural order
+        System.out.println("TreeSet elements:");
+        for (Integer a : num) {
+            System.out.println(a);
+        }
+
+        // Displaying the smallest element
+        System.out.println("Smallest element: " + num.first());
+
+        // Displaying the largest element
+        System.out.println("Largest element: " + num.last());
+    }
 }
